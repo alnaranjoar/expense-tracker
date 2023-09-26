9933e86d876f1653047dd9c35bd699fbd93b64c5
Alejandro Naranjo <anaranjoa93@gmail.com>
2023-09-25 19:08:05 -0500

    Add a New Expense Button added. Expense form renders dinamically if I click the Add a New Expense Button and it hides when the new expense is added or if I cancel the addition of the new expense

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 import './Expenses.css'
 import Card from '../UI/Card';
-import ExpenseItem from './ExpenseItem';
 import ExpensesFilter from './ExpensesFilter';
+import ExpensesList from './ExpensesList';
 
 function Expenses(props) {
     const [selectedYear, changeSelectedYear] = useState('All')
@@ -18,20 +18,13 @@ function Expenses(props) {
         filteredExpenses = props.expenses.filter(expense => String(expense.date.getFullYear()) === selectedYear)
     }
 
-    let expensesContent = <p>No expenses found.</p>
-
-    if(filteredExpenses.length > 0) {
-        expensesContent = filteredExpenses.map(expense => 
-            <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date} />
-        )
-    }
-
     return(
         <Card className='expenses'>
             <ExpensesFilter selected = {selectedYear} onFilteredYearChange = {saveFilteredYear}/>
-            {expensesContent}
+            <ExpensesList items={filteredExpenses}/>
         </Card>
     )
+
 }
 
 export default Expenses;
